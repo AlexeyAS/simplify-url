15ff487701a304081c4cefd66853a4b98ae7637b
Alexey AS <heretik4@gmail.com>
2019-03-19 15:49:39 +0300

    v-test-1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Сокращатель ссылок</title>
+    </head>
+    <body>
+    <form action="" method="post">
+        <input type="url" required placeholder="Введите ссылку..." autocomplete="off" name="url">
+        <input type="submit" name="submit" value="Сократить">
+    </form>
+
+    <?php require "link.php" ?>
+
+    </body>
+    </html>
