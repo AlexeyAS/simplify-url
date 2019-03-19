d0a872312fa64be213ef3d6b5572e0fc4fdd3944
Alexey AS <heretik4@gmail.com>
2019-03-19 17:55:31 +0300

    test-psr-1

--- link.php
+++ link.php
@@ -1,15 +1,23 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+
 $h = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm1234567890"; //Выбираем символы, из которых будет состоять наш рандом
 $rand = substr(str_shuffle($h), 0, 5); //создаём ранд. Цифра 5 обозначает длину ранда
 $site = "links/"; //Адрес вашего сайта. Слэш обязателен!
 $url = $_POST['url'];
-if ($_POST['submit']) {
-    echo "<a href='$site$rand.php' target='_blank'> Адрес сайта/$rand</a>"; //выводим пользователю ссылку, в виде ссылки
-    $f = fopen("links/$rand.php", "w"); //Создаём файл с именем ранда. Я решил скидывать файлы в каталог a, поэтому не забудьте его создать!
-    fwrite($f, "<?php header('Location: $url') ?>"); //И записываем в код редиректа, с ссылкой которую ввёл пользователь
-    fclose($f); //Закрываем файл
-    $fh = fopen(".htaccess", "a"); //Открываем файл .htaccess с дозаписью на последний байт
-    fwrite($fh, "
+
+function submit($site, $rand, $url){
+    if ($_POST['submit']) {
+        echo "<a href='$site$rand.php' target='_blank'> Адрес сайта/$rand</a>"; //выводим пользователю ссылку, в виде ссылки
+        $fLink = fopen("links/$rand.php", "w"); //Создаём файл с именем ранда. Я решил скидывать файлы в каталог a, поэтому не забудьте его создать!
+        fwrite($fLink, "<?php header('Location: $url') ?>"); //И записываем в код редиректа, с ссылкой которую ввёл пользователь
+        fclose($fLink); //Закрываем файл
+        $fHt = fopen(".htaccess", "a"); //Открываем файл .htaccess с дозаписью на последний байт
+        fwrite($fHt, "
     RewriteRule ^$rand$ /links/$rand.php"); //Записываем ссылку на файл в каталоге links и её сокращённый вариант, который был дан пользователю. !ВНИМАНИЕ! Перенос сделан специально, иначе всё будет писаться в плотную и вызовет 500 ошибку!
-    fclose($fh); //Закрываем файл
+        fclose($fHt); //Закрываем файл
+    }
+}
+if(isset($_POST['submit'])){
+    submit($site, $rand, $url);
 }
