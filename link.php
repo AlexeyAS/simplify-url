abd51db130a31186f4338a50a4cb68f14a7f505f
Alexey AS <heretik4@gmail.com>
2019-03-20 00:34:25 +0300

    psr-2

--- link.php
+++ link.php
@@ -1,15 +1,35 @@
 <?php
-$h = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm1234567890"; //Выбираем символы, из которых будет состоять наш рандом
-$rand = substr(str_shuffle($h), 0, 5); //создаём ранд. Цифра 5 обозначает длину ранда
-$site = "links/"; //Адрес вашего сайта. Слэш обязателен!
+header('Content-Type: text/html; charset=utf-8');
+/*Выбираем символы, из которых будет состоять наша рандомная ссылка.*/
+$symb = "QqWwAaSsDdFfGgHhJjKkLlZzEeRrTtYyUuIiOoPpXxCcVvBbNnMm1234567890";
+/*Создаём рандом. Цифра 4 обозначает длину.*/
+$rand = substr(str_shuffle($symb), 0, 4);
+/*Адрес вашего сайта.*/
+$site = "links/"; //Слэш обязателен!
 $url = $_POST['url'];
-if ($_POST['submit']) {
-    echo "<a href='$site$rand.php' target='_blank'> Адрес сайта/$rand</a>"; //выводим пользователю ссылку, в виде ссылки
-    $f = fopen("links/$rand.php", "w"); //Создаём файл с именем ранда. Я решил скидывать файлы в каталог a, поэтому не забудьте его создать!
-    fwrite($f, "<?php header('Location: $url') ?>"); //И записываем в код редиректа, с ссылкой которую ввёл пользователь
-    fclose($f); //Закрываем файл
-    $fh = fopen(".htaccess", "a"); //Открываем файл .htaccess с дозаписью на последний байт
-    fwrite($fh, "
-    RewriteRule ^$rand$ /links/$rand.php"); //Записываем ссылку на файл в каталоге links и её сокращённый вариант, который был дан пользователю. !ВНИМАНИЕ! Перенос сделан специально, иначе всё будет писаться в плотную и вызовет 500 ошибку!
-    fclose($fh); //Закрываем файл
+
+function submit($site, $rand, $url)
+{
+    if ($_POST['submit']) {
+        /*Вывод ссылки пользователю*/
+        echo "<a href='$site$rand.php' target = '_blank'> Адрес сайта/$rand</a>";
+        /*Создаём файл с именем рандомной ссылки в каталоге links*/
+        $fLink = fopen("links/$rand.php", "w");
+        /*Записываем в код редиректа, с ссылкой которую ввёл пользователь*/
+        fwrite($fLink, "<?php header('Location: $url') ?>");
+        /*Закрываем файл в папке links*/
+        fclose($fLink);
+        /*Открываем файл .htaccess с дозаписью на последний байт*/
+        $fHt = fopen(".htaccess", "a");
+        /*Записываем ссылку на файл в каталоге links и её сокращённый вариант, который был дан пользователю.*/
+        //!ВНИМАНИЕ! Перенос сделан специально, иначе всё будет писаться в плотную и вызовет 500 ошибку!
+        fwrite($fHt, "
+    RewriteRule ^$rand$ /links/$rand.php");
+        /*Закрываем файл*/
+        fclose($fHt);
+    }
+}
+
+if (isset($_POST['submit'])) {
+    submit($site, $rand, $url);
 }
